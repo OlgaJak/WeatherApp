81301763de2959eaf209e76d39b7e6f072d96a4e
OlgaJak <jakimenko83@mail.ru>
2023-05-01 16:47:17 +0300

    fixed problem with nod adding data to tables, overwriting data

---
+++ src/main/java/TeamOpav/DataBaseActions/DBandTable/ShowTable.java
@@ -0,0 +1,28 @@
+package TeamOpav.DataBaseActions.DBandTable;
+
+import TeamOpav.DataBaseActions.DBClass.Location;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import jakarta.persistence.TypedQuery;
+
+import java.util.List;
+
+public class ShowTable {
+    public static void displayTable() {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("weather_app");
+        EntityManager em = emf.createEntityManager();
+
+        TypedQuery<Location> query = em.createQuery("SELECT l FROM Functions.Location l", Location.class);
+        List<Location> locations = query.getResultList();
+        System.out.println(String.format("%15S","id | cityName | longitude | latitude | region | countryName"));
+        for (Location location : locations) {
+            System.out.println(String.format("%15s",
+                    location.getId() + " | " + location.getCityName() + " | " + location.getLatitude() + "| " + location.getLongitude() + " | " + location.getRegion() + " | " + location.getCountryName()));
+        }
+
+        em.close();
+        emf.close();
+    }
+}

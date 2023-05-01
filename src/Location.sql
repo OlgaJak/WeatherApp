bb8a9c62447c8f359753abae4f33346245d8e92a
OlgaJak <jakimenko83@mail.ru>
2023-05-01 17:29:21 +0300

    deleted unnecessary data

--- src/Location.sql
+++ src/Location.sql
@@ -5,5 +5,12 @@ longitude double DEFAULT NULL,
 city_name varchar(255) DEFAULT NULL,
 country_name varchar(255) NOT NULL,
 region varchar(255) NOT NULL,
+city_name VARCHAR(255) NOT NULL,
+temperature double NOT NULL,
+pressure double NOT NULL,
+humidity double NOT NULL,
+wind_direction varchar(100) NOT NULL,
+speed double NOT NULL,
+PRIMARY KEY (id),
 PRIMARY KEY (`id`)
 );
